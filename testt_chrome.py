e7b767c7886b8e106630eb7377dd8fdaa744c739
Takshil <takshil@letsenkindle.com>
2024-12-25 15:39:52 +0530

    25/12/2024

--- testt_chrome.py
+++ testt_chrome.py
@@ -1,19 +1,16 @@
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 
 def init_driver():
     chrome_options = Options()
+    chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--headless=new")  # Use new headless mode
     chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--remote-debugging-port=9222")
-    chrome_options.add_argument("--disable-software-rasterizer")
 
-    # Automatically download and use the correct ChromeDriver
-    service = Service(ChromeDriverManager().install())
+    service = Service(ChromeDriverManager(version="130.0.6723.69").install())  # Specify version
     driver = webdriver.Chrome(service=service, options=chrome_options)
     return driver
 
