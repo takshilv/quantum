5c77e0f15a5a0cb26e0dd5290b877892ef556353
Takshil <takshil@letsenkindle.com>
2024-12-25 15:49:56 +0530

    25/12/2024

--- qml_integration_development.py
+++ qml_integration_development.py
@@ -28,6 +28,15 @@ class ApplicationProcessor:
             chrome_options.add_argument("--disable-gpu")
             chrome_options.add_argument("--disable-software-rasterizer")
             chrome_options.add_argument("--disable-setuid-sandbox")
+        custom_cache_path = os.path.expanduser("~/.wdm")
+        os.environ['WDM_LOCAL'] = custom_cache_path
+
+        # Ensure the directory exists
+        if not os.path.exists(custom_cache_path):
+            os.makedirs(custom_cache_path)
+
+        print(f"WebDriverManager cache directory: {custom_cache_path}")
+
         service = Service(ChromeDriverManager().install())
         driver = webdriver.Chrome(service=service, options=chrome_options)
         #     # chrome_options = Options()
